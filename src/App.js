c6b40072db2bc89641a0507bfefaeb574551fc31
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-26 20:24:41 +0000

    rage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Footer.css";
@@ -16,27 +16,6 @@ class App extends React.Component {
     user: {},
   };
 
-  componentDidMount = () => {
-    this.fetchProfile();
-  };
-
-  fetchProfile = async () => {
-    try {
-      let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/profile/me",
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_BE_URL}`,
-          },
-        }
-      );
-      let parsedResponse = await response.json();
-
-      this.setState({ user: parsedResponse });
-    } catch (error) {
-      console.log(error);
-    }
-  };
   render() {
     return (
       <BrowserRouter>

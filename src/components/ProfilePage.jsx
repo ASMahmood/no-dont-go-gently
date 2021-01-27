2ca4d68da905f52f5d4fe83b3e42cda36f5afd53
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-27 07:33:45 +0000

    adding rest of profile page

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import RightSideColumn from "./RightSideColumn";
+import ProfileLeft from "./ProfileLeft";
 
 const ProfilePage = () => {
   return (
     <>
       <Container style={{ marginTop: "4rem" }}>
         <Row>
+          <ProfileLeft />
           <RightSideColumn />
         </Row>
         <hr />
